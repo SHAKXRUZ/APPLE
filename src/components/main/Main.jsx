e93449346beabbcec7146259ac731171ab3cc68e
Davronbek <davroncoder074@gmail.com>
2024-06-24 12:16:59 +0500

    salom

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,12 @@
 import "./Main.css";
-
+import logo from "./assetss/main_img/images/photo_5461125233139387647_x-removebg-preview.png"
 const Main = () => {
-  return <div>Main</div>;
+  return <div className="container_main">
+    <div className="rangs">
+    <img src={logo} alt="" className="imggg"/>
+    <button className="btn_main">Shop</button>
+    </div>
+  </div>;
 };
 
 export default Main;
